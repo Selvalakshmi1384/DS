a039b01848b191961f630144e2e9f45034896863
Selvalakshmi1384 <selvalakshmid.ug.21.cs@francisxavier.ac.in>
2023-06-23 12:35:07 +0530

    gg

--- my-react-app/src/Page1.js
+++ my-react-app/src/Page1.js
@@ -4,7 +4,7 @@ import './Page1.css';
 function Page1(){
     return(
 <div className="page1">
-    <p><h1>WELfCOME TO MY PORTFOLIO!</h1><br/>Explore my creative journey and professional accomplishments.
+    <p><h1>WELCOME TO MY PORTFOLIO!</h1><br/>Explore my creative journey and professional accomplishments.
 A showcase of <br/>my skills, experience, and passion.</p>
 </div>
     );
